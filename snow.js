d4cc0857c7c5be4219994f3f2c21d99fbe3cc5c3
ying345 <3217336367@qq.com>
2020-04-16 12:53:48 +0800

    Site updated: 2020-04-16 12:53:47

--- snow.js
+++ snow.js
@@ -1,48 +1,23 @@
-/*样式一*/
-(function ($) {
-    $.fn.snow = function (options) {
-        var $flake = $('<div id="snowbox" />').css({
-                'position': 'absolute',
-                'z-index': '9999',
-                'top': '-50px'
-            }).html('❄'),
-            documentHeight = $(document).height(),
-            documentWidth = $(document).width(),
-            defaults = {
-                minSize: 10,
-                maxSize: 20,
-                newOn: 1000,
-                flakeColor: "#FFFFFF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
-            },
-            options = $.extend({}, defaults, options);
-        var interval = setInterval(function () {
-            var startPositionLeft = Math.random() * documentWidth - 100,
-                startOpacity = 0.5 + Math.random(),
-                sizeFlake = options.minSize + Math.random() * options.maxSize,
-                endPositionTop = documentHeight - 200,
-                endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
-                durationFall = documentHeight * 10 + Math.random() * 5000;
-            $flake.clone().appendTo('body').css({
-                left: startPositionLeft,
-                opacity: startOpacity,
-                'font-size': sizeFlake,
-                color: options.flakeColor
-            }).animate({
-                top: endPositionTop,
-                left: endPositionLeft,
-                opacity: 0.2
-            }, durationFall, 'linear', function () {
-                $(this).remove()
-            });
-        }, options.newOn);
-    };
-})(jQuery);
 $(function () {
-    $.fn.snow({
-        minSize: 5,
-        /* 定义雪花最小尺寸 */
-        maxSize: 50,
-        /* 定义雪花最大尺寸 */
-        newOn: 300 /* 定义密集程度，数字越小越密集 */
-    });
+    setInterval(function() {
+        var maxW = document.body.clientWidth,
+            maxH = document.body.clientHeight,
+            left = Math.random() * maxW,
+            bottom = left - (Math.random() - 0.5) * 0.2 * maxW, //保证落下的位置水平有变化，但不大
+            opacity = 0.7 + 0.3 * Math.random();
+        speed = 0.1;
+        size = 20 + 10 * Math.random(), //字体20-30
+            color = '#fff';
+        // num = Math.floor(Math.random() * 10) //产生0-9随机数，当然你们可以自己设置
+        num ='*';
+        var style = 'position:absolute;top:0px;font-size:' + size + 'px;color:' + color + ';left:' + left + 'px;opacity:' + opacity;
+        var div = '<span class = "dd" style="' + style + '">' + '<img src="/snow.png" />' + '</span>'
+        $('body').append(div)
+        $('span').animate({
+            top: maxH,
+            left: bottom
+        }, 3000, function() {
+            $(this).remove() //这一步很关键，要把落下的去掉，不然会越积越多
+        });
+    }, 20) //20ms产生一个
 });
